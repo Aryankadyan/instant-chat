389cc3375fa59cc89cd423d75b9503597dbc5440
jai-max <kadyanaryan744@gmail.com>
2025-02-26 19:44:56 +0530

    Chat Container

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,50 @@
+import {create} from 'zustand'
+import toast from "react-hot-toast"
+import {axiosInstance} from '../library/axios'
+
+
+export const useChatStore = create((set) =>({
+message: [],
+users: [],
+selectedUsers: null,
+isUsersLoading: false,
+isMessagesLoading: false,
+
+getUsers: async() =>{
+    set({ isUsersLoading: true})
+    try{
+        const res = await axiosInstance.get('/messages/users')
+        set({users: res.data})
+    }catch(error){
+        toast.error(error.response.data.message)
+    }finally{
+        set({isUsersLoading: false})
+    }
+},
+
+getMessages: async(userId)=>{
+    set({ isMessagesLoading: true})
+    try {
+        const res = await axiosInstance.get(`/messages/${userId}`)
+        set({messages: res.data})
+
+    } catch (error) {
+        toast.error(error.response.data.message)
+    } finally{
+        set({isMessagesLoading: false})
+    }
+},
+
+sendMessage: async(messageData) => {
+   const { selectedUser, messages } = useChatStore.getState()
+   try {
+    const res = await axiosInstance.post(`/messages/send${selectedUser._id}`, messageData)
+    set({messages:[...messages, res.data]})
+   } catch (error) {
+    toast.error(error.response.data.message)
+    
+   }
+},
+  
+setSelectedUser: (selectedUser) => set({selectedUser}),
+}))
